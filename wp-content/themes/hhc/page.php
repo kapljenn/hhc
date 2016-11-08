cedc27c3c91a8bf50ac67bb52bb01dd066736a8d
Unique Digital <uniquestash@syzygy.co.uk>
2016-11-08 15:41:19 +0000

    added footer

--- wp-content/themes/hhc/page.php
+++ wp-content/themes/hhc/page.php
@@ -94,28 +94,28 @@ while ( have_posts() ) : the_post();
 			<div class="fix-3-12">
 				<img src="#">
 				<h3 class="equalElement ae-7">Meet Quick</h3>
-				<div class="ae-8">Here is some column text.</div>
+				<div class="ae-5">Here is some column text.</div>
 			</div>
 		</li>
 		<li class="col-3-12">
 			<div class="fix-3-12">
 				<img src="#">
 				<h3 class="equalElement ae-8">Nice Experience</h3>
-				<div class="ae-9">Here is some column text.</div>
+				<div class="ae-6">Here is some column text.</div>
 			</div>
 		</li>
 		<li class="col-3-12">
 			<div class="fix-3-12">  
 				<img src="#">
 				<h3 class="equalElement ae-9">Simple Beauty</h3>
-				<div class="ae-10">Here is some column text.</div>
+				<div class="ae-7">Here is some column text.</div>
 			</div>
 		</li>
 		<li class="col-3-12">
 			<div class="fix-3-12">  
 				<img src="#">
-				<h3 class="equalElement ae-9">Simple Beauty</h3>
-				<div class="ae-11">Here is some column text.</div>
+				<h3 class="equalElement ae-10">Simple Beauty</h3>
+				<div class="ae-8">Here is some column text.</div>
 			</div>
 		</li>
 	</ul>
@@ -182,6 +182,68 @@ while ( have_posts() ) : the_post();
 	endif;
 
 endwhile;
+
+?>
+
+<section class="slide fade kenBurns footer"><div class="content"><div class="container"><div class="wrap">
+	<div class="fix-12-12">
+		<ul class="grid later equal">
+			<li class="col-3-12">
+				<div class="fix-3-12">
+					<h3 class="equalElement ae-7">Lorem Ipsum</h3>
+					<div class="ae-5">Here is some column text.</div>
+				</div>
+			</li>
+			<li class="col-3-12">
+				<div class="fix-3-12">
+					<h3 class="equalElement ae-8">Lorem Ipsum</h3>
+					<div class="ae-6">Here is some column text.</div>
+				</div>
+			</li>
+			<li class="col-3-12">
+				<div class="fix-3-12">  
+					<h3 class="equalElement ae-9">Lorem Ipsum</h3>
+					<div class="ae-7">Here is some column text.</div>
+				</div>
+			</li>
+			<li class="col-3-12">
+				<div class="fix-3-12">
+					<h3 class="equalElement ae-10">Lorem Ipsum</h3>
+					<div class="ae-8">Here is some column text.</div>
+				</div>
+			</li>
+		</ul>
+	</div>
+	<div class="fix-10-12 toCenter">
+		<br><br><br><br>
+		<h3 class="equalElement ae-10">Follow us</h3>
+		<ul class="social ae-9">
+			<li>
+				<a href="#">
+					<img src="<?php echo esc_url(get_stylesheet_directory_uri()); ?>/img/facebook.png" />
+				</a>
+			</li>
+			<li>
+				<a href="#">
+					<img src="<?php echo esc_url(get_stylesheet_directory_uri()); ?>/img/twitter.png" />
+				</a>
+			</li>
+			<li>
+				<a href="#">
+					<img src="<?php echo esc_url(get_stylesheet_directory_uri()); ?>/img/instagram.png" />
+				</a>
+			</li>
+		</ul>
+	</div>
+</section>
+
+
+
+
+
+
+<?php
+
 
 get_footer();
 
